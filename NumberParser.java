ffe2455640051348856e8be23cfb6102b9aa137f
Matt Weeks <m_weeks@hotmail.co.uk>
2021-12-03 23:23:27 +0000

    Add required class.

---
+++ NumberParser.java
@@ -0,0 +1,10 @@
+public class NumberParser {
+    public NumberParser(Map<String, Integer> countryCodes, Map<String, String> nationalTrunkPrefixes) {
+        // TODO
+    }
+
+    public String parse(String dialledNumber, String userNumber) {
+        // TODO
+        return "";
+    }
+}
