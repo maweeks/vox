471932bd32d33fae5836ddc9f2fd06084232043a
Matt Weeks <m_weeks@hotmail.co.uk>
2021-12-05 22:40:57 +0000

    Improve testing.

--- src/main/java/NumberParser.java
+++ src/main/java/NumberParser.java
@@ -7,15 +7,23 @@ public class NumberParser {
     private Map<String, Integer> countryCodes;
     private Map<Integer, String> reversedCountryCodes;
     private Map<String, String> nationalTrunkPrefixes;
+    private int maxCountryCodeLength;
 
     public NumberParser(Map<String, Integer> countryCodes, Map<String, String> nationalTrunkPrefixes) {
         this.countryCodes = countryCodes;
         this.nationalTrunkPrefixes = nationalTrunkPrefixes;
+        this.maxCountryCodeLength = 0;
         this.reversedCountryCodes = reverseCountryCodes(countryCodes);
+
+    }
+
+    public Map<Integer, String> getReversedCountryCodes() {
+        return this.reversedCountryCodes;
     }
 
     private Map<Integer,String> reverseCountryCodes(Map<String, Integer> countryCodes) {
         Map<Integer,String> codes = new HashMap<Integer, String>();
+        int maxCountryCode = 0;
 
         Iterator<Entry<String, Integer>> iterator
             = countryCodes.entrySet().iterator();
@@ -23,9 +31,17 @@ public class NumberParser {
         while (iterator.hasNext()) {
             Map.Entry<String, Integer> map
                 = (Map.Entry<String, Integer>)
-                      iterator.next();
+                    iterator.next();
             codes.putIfAbsent(map.getValue(), map.getKey());
+
+            if (map.getValue() > maxCountryCode) {
+                maxCountryCode = map.getValue();
+            }
+
         }
+
+        this.maxCountryCodeLength = Integer.toString(maxCountryCode).length();
+
         return codes;
     }
 
@@ -38,14 +54,14 @@ public class NumberParser {
         userNumber = userNumber.replaceAll("\\s+","");
 
         String userLocation = getUserLocation(userNumber);
-        if (userLocation.length() > 0) {
+        if (userLocation != "") {
             return "+" + countryCodes.get(userLocation) + dialledNumber.substring((nationalTrunkPrefixes.get(userLocation)).length());
         }
         return dialledNumber;
     }
 
     private String getUserLocation(String userNumber){
-        for (int i = 0; i < 4; i++) { // Assuming max country code length of 4
+        for (int i = 0; i < this.maxCountryCodeLength; i++) {
             int possibleCountryCode = Integer.parseInt(userNumber.substring(1, i + 2));
             String value = this.reversedCountryCodes.get((possibleCountryCode));
             if (value != null) {
--- src/test/java/TestNumberParser.java
+++ src/test/java/TestNumberParser.java
@@ -16,12 +16,14 @@ public class TestNumberParser {
     public void before() {
         countryCodes = new HashMap<String, Integer>();
         countryCodes.put("UK", 44);
+        countryCodes.put("FAKE", 43);
         countryCodes.put("FR", 33);
         countryCodes.put("US", 1);
         countryCodes.put("HK", 852);
 
         nationalTrunkPrefixes = new HashMap<String, String>();
         nationalTrunkPrefixes.put("UK", "0");
+        nationalTrunkPrefixes.put("FAKE", "136");
         nationalTrunkPrefixes.put("FR", "0");
         nationalTrunkPrefixes.put("US", "1");
         nationalTrunkPrefixes.put("HK", "");
@@ -30,32 +32,41 @@ public class TestNumberParser {
     }
 
     @Test
-    public void testUkToUk(){
+    public void testReverseCountryCodes(){
+        Map<Integer, String> expected = new HashMap<Integer, String>();
+        expected.put(44, "UK");
+        expected.put(43, "FAKE");
+        expected.put(33, "FR");
+        expected.put(1, "US");
+        expected.put(852, "HK");
+
+        Assert.assertEquals(expected, parser.getReversedCountryCodes());
+    }
+
+    @Test
+    public void testParseUkToUk(){ // Test case from task
         String dialledNumber = "07277822334";
         String userNumber = "+447866866886";
 
         String expected = "+447277822334";
-
         Assert.assertEquals(expected, parser.parse(dialledNumber, userNumber));
     }
 
     @Test
-    public void testUsToUs(){
+    public void testParseUsToUs(){ // Test case from task
         String dialledNumber = "1312233244";
         String userNumber = "+1212233200";
 
         String expected = "+1312233244";
-
         Assert.assertEquals(expected, parser.parse(dialledNumber, userNumber));
     }
 
     @Test
-    public void testUkToUs(){
+    public void testParseUkToUs(){ // Test case from task
         String dialledNumber = "+1312233244";
         String userNumber = "+447866866886";
 
         String expected = "+1312233244";
-
         Assert.assertEquals(expected, parser.parse(dialledNumber, userNumber));
     }
 }
